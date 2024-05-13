295adae45f0a5a57ef3577747cd7ac276efcbfcb
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-13 20:28:53 +0700

    refactor: extract icons to separate file

--- src/ui/header-search.tsx
+++ src/ui/header-search.tsx
@@ -1,3 +1,5 @@
+import { SearchIcon } from "./icons";
+
 // TODO: Mobile first responsive design for search bar
 export default function HeaderSearch() {
   return (
@@ -7,21 +9,7 @@ export default function HeaderSearch() {
       </label>
       <div className="relative flex rounded-md border bg-bg_light pe-2 ps-3 focus-within:ring">
         <div className="pointer-events-none flex items-center">
-          <svg
-            className="size-4 text-gray-500 dark:text-gray-400"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
-          </svg>
+          <SearchIcon />
         </div>
         <input
           type="search"
