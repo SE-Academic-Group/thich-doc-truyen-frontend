a003c0a4efaa6aa7f7e495bee9c43ef189d82755
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-13 20:34:57 +0700

    style: align text left

--- src/ui/layout/banner.tsx
+++ src/ui/layout/banner.tsx
@@ -2,9 +2,7 @@ export default function Banner() {
   return (
     <aside className="border-y py-1 shadow">
       <div className="container">
-        <p className="text-center">
-          Đọc truyện online, đọc truyện chữ, truyện full, truyện hay.
-        </p>
+        <p>Đọc truyện online, đọc truyện chữ, truyện full, truyện hay.</p>
       </div>
     </aside>
   );
