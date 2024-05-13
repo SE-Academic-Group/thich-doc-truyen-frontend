58322606d78a1a2a8c9be4f38c4100c30b452da8
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-13 20:42:02 +0700

    feat: create search page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
 export default function Page() {
   return (
-    <main className="container">
-      <h1>Home</h1>
+    <main className="py-8">
+      <div className="container">
+        <h1 className="sr-only">Trang chủ Thích Đọc Truyện</h1>
+        <section>
+          <h2 className="text-xl font-semibold uppercase underline underline-offset-4">
+            Tìm kiếm truyện
+          </h2>
+          <div>search</div>
+        </section>
+        <div className="grid grid-cols-2 gap-2">
+          <section>
+            <h2 className="text-xl font-semibold uppercase underline underline-offset-4">
+              Truyện đã đọc xong
+            </h2>
+            <div>Danh sach</div>
+          </section>
+          <section>
+            <h2 className="text-xl font-semibold uppercase underline underline-offset-4">
+              Truyện đang đọc
+            </h2>
+            <div>Danh sach</div>
+          </section>
+        </div>
+      </div>
     </main>
   );
 }
