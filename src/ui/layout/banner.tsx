41837c8cf8f670e337aae15c24b88d7d2b7957b5
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-13 20:32:48 +0700

    feat: create banner

---
+++ src/ui/layout/banner.tsx
@@ -0,0 +1,11 @@
+export default function Banner() {
+  return (
+    <aside className="border-y py-1 shadow">
+      <div className="container">
+        <p className="text-center">
+          Đọc truyện online, đọc truyện chữ, truyện full, truyện hay.
+        </p>
+      </div>
+    </aside>
+  );
+}
