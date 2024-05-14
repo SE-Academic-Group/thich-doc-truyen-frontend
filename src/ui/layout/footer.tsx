c2bb00a11d3b2bb6f0a635725f572fac38c5b4f2
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-14 10:21:01 +0700

    style: add copyright to the right

--- src/ui/layout/footer.tsx
+++ src/ui/layout/footer.tsx
@@ -2,14 +2,19 @@ import ThichDocTruyenLogo from "../thich-doc-truyen-logo";
 
 export default function Footer() {
   return (
-    <footer className="border-y py-2 shadow">
-      <div className="container">
-        <ThichDocTruyenLogo size="sm" />
-        <p className="max-w-prose text-pretty">
-          Đọc truyện quá 60 phút có hại cho sức khỏe, đặc biệt là mắt của
-          bạn.&nbsp;
-          <br />
-          Hãy đọc truyện mà bạn thích, đừng đọc truyện vì bạn thích.
+    <footer className="border-y py-3 shadow">
+      <div className="container grid grid-cols-2 items-center gap-4">
+        <div>
+          <ThichDocTruyenLogo size="sm" />
+          <p className="max-w-prose text-pretty">
+            Đọc truyện quá 60 phút có hại cho sức khỏe, đặc biệt là mắt của
+            bạn.&nbsp;
+            <br />
+            Hãy đọc truyện mà bạn thích, đừng đọc truyện vì bạn thích.
+          </p>
+        </div>
+        <p className="text-center">
+          Copyright &copy; 2024 FIT_HCMUS ANH EM XA DOAN
         </p>
       </div>
     </footer>
