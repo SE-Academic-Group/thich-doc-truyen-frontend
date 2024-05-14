ad4b65c4fe72e0027693ad0131981cc2e2442be9
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-14 16:40:11 +0700

    fix: remove double Root

--- src/ui/home/search-source-info.tsx
+++ src/ui/home/search-source-info.tsx
@@ -11,26 +11,24 @@ export default function SearchSourceInfo() {
         <span className="font-bold uppercase text-primary">truyenfull.vn</span>
       </div>
       <Popover.Root>
-        <Popover.Root>
-          <Popover.Trigger>
-            <div className="flex items-center gap-0.5">
-              <QuestionIcon />
-              <span>Thay đổi nguồn</span>
+        <Popover.Trigger>
+          <div className="flex items-center gap-0.5">
+            <QuestionIcon />
+            <span>Thay đổi nguồn</span>
+          </div>
+        </Popover.Trigger>
+        <Popover.Portal>
+          <Popover.Content>
+            <div className="rounded-md border bg-white px-4 py-2 shadow-lg">
+              Tùy chỉnh
+              <span aria-label="Bước tiếp theo"> &rarr; </span>
+              Thay đổi nguồn <br className="md:hidden" />
+              <span aria-label="Bước tiếp theo"> &rarr; </span>
+              Tích chọn nguồn truyện
             </div>
-          </Popover.Trigger>
-          <Popover.Portal>
-            <Popover.Content>
-              <div className="rounded-md border bg-white px-4 py-2 shadow-lg">
-                Tùy chỉnh
-                <span aria-label="Bước tiếp theo"> &rarr; </span>
-                Thay đổi nguồn <br className="md:hidden" />
-                <span aria-label="Bước tiếp theo"> &rarr; </span>
-                Tích chọn nguồn truyện
-              </div>
-              <Popover.Arrow />
-            </Popover.Content>
-          </Popover.Portal>
-        </Popover.Root>
+            <Popover.Arrow />
+          </Popover.Content>
+        </Popover.Portal>
       </Popover.Root>
     </div>
   );
