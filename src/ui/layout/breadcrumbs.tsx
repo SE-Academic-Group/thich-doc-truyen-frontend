c9161688b04474cc7b434e127ab299d7f2721b3a
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-15 09:44:58 +0700

    style: make text size -> sm

--- src/ui/layout/breadcrumbs.tsx
+++ src/ui/layout/breadcrumbs.tsx
@@ -1,6 +1,6 @@
 export default function Breadcrumbs() {
   return (
-    <aside className="border-y py-1 shadow">
+    <aside className="border-y py-1 text-sm shadow">
       <div className="container">trang chu / tim kiem tu khoa: Yeu than</div>
     </aside>
   );
