d1b668e76e61773cfa734fec6431dcd5a96cdcbf
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-15 21:29:24 +0700

    fix: resolve conflict

--- src/app/tim-kiem/page.tsx
+++ src/app/tim-kiem/page.tsx
@@ -1,23 +1,3 @@
-import Breadcrumbs from "@/ui/layout/breadcrumbs";
-
-export default function Page({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  const q = (searchParams["q"] ?? "").toString();
-
-  return (
-    <div>
-      <Breadcrumbs
-        breadcrumbs={[
-          {
-            label: "Tìm kiếm truyện: " + q,
-            href: "/tim-kiem?q=" + q.replace(/ /g, "+"),
-          },
-        ]}
-      />
-      <main className="container py-4">Page</main>
-    </div>
-  );
+export default function Page() {
+  return <main className="container py-4">Page</main>;
 }
