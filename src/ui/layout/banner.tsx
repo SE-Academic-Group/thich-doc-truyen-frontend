c92e6fbc98c0ab8f978ff20f17a0ae7919dc3adc
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-15 09:48:25 +0700

    fix: add  to the text

--- src/ui/layout/banner.tsx
+++ src/ui/layout/banner.tsx
@@ -2,7 +2,7 @@ export default function Banner() {
   return (
     <aside className="border-y py-1 shadow">
       <div className="container text-sm">
-        <p>Đọc truyện online, đọc truyện chữ, truyện full, truyện hay.</p>
+        <p>Đọc truyện online, đọc truyện chữ, truyện full, và truyện hay.</p>
       </div>
     </aside>
   );
