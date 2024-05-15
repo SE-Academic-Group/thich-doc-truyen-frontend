238512fd58791fef071767a9730e4ae4e0b7a637
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-15 09:47:49 +0700

    style: remove search button

--- src/ui/header-search.tsx
+++ src/ui/header-search.tsx
@@ -1,31 +1,24 @@
 import { searchAction } from "@/lib/actions";
 import { SearchIcon } from "./icons";
 
-// TODO: Mobile first responsive design for search bar
 export default function HeaderSearch() {
   return (
-    <form className="hidden max-w-xs grow md:block" action={searchAction}>
+    <form className="hidden max-w-64 grow md:block" action={searchAction}>
       <label htmlFor="header-search-input" className="sr-only">
-        Search
+        Tìm kiếm truyện theo tên
       </label>
-      <div className="relative flex rounded-md border bg-bg_light pe-1 ps-3 focus-within:ring">
+      <div className="relative flex rounded-md border bg-bg_light pe-1 ps-3 text-fg_light focus-within:ring">
         <div className="pointer-events-none flex items-center">
-          <SearchIcon />
+          <SearchIcon className="size-3" />
         </div>
         <input
           type="search"
           name="search-input"
           id="header-search-input"
-          className="grow rounded-lg bg-bg_light px-2.5 py-1 text-fg_light placeholder:text-sm focus:outline-none"
+          className="grow rounded-sm px-2.5 py-1 placeholder:text-sm focus:outline-none md:py-1.5 md:text-sm"
           placeholder="Tìm kiếm theo tên truyện..."
           required
         />
-        <button
-          type="submit"
-          className="self-center rounded-md bg-primary px-2 py-1 text-sm"
-        >
-          Tìm kiếm
-        </button>
       </div>
     </form>
   );
