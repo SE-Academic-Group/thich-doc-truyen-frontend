6b4eb813992a74bfbf9511c91c3bcb24f6b8f26d
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-16 21:15:15 +0700

    chore: add mechuyenchu hostname to host image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "metruyenchu.com.vn",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
