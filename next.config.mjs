e53a7b7a7df1131d97a4cad7fc72762fed85856c
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-20 12:03:27 +0700

    chore: enable all sources of image end with .com

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "metruyenchu.com.vn",
-      },
-      {
-        protocol: "https",
-        hostname: "www.nae.vn",
+        hostname: "**.com",
       },
     ],
   },
