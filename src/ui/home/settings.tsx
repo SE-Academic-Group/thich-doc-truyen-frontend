e29d4c76b6e98d1fc174cd9c3a1e03915c050c94
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-27 10:58:34 +0700

    refactor: abstract away popover from radix ui

--- src/ui/home/settings.tsx
+++ src/ui/home/settings.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { BarsIcon } from "@/lib/icons";
-import * as Popover from "@radix-ui/react-popover";
+import { Popover } from "@/ui/common/popover";
 
 export default function Settings() {
   return (
@@ -9,20 +9,13 @@ export default function Settings() {
       <Popover.Trigger className="rounded-full p-1.5 hover:bg-bg-100">
         <BarsIcon className="size-7" />
       </Popover.Trigger>
-      <Popover.Portal>
-        <Popover.Content
-          side="bottom"
-          align="end"
-          sideOffset={8}
-          className="rounded-md border bg-white px-4 py-2 shadow-lg"
-        >
-          <ul>
-            <li>
-              <span>thay doi nguon truyen</span>
-            </li>
-          </ul>
-        </Popover.Content>
-      </Popover.Portal>
+      <Popover.Content>
+        <ul>
+          <li>
+            <span>thay doi nguon truyen</span>
+          </li>
+        </ul>
+      </Popover.Content>
     </Popover.Root>
   );
 }
