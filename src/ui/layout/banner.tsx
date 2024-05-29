446cd8b20220f81f8866086cd05fb356fb25be61
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-29 21:00:06 +0700

    chore: remove old and unused layout components

--- src/ui/layout/banner.tsx
+++
@@ -1,9 +0,0 @@
-export default function Banner() {
-  return (
-    <aside className="border-y py-1 shadow-sm">
-      <div className="container text-sm">
-        <p>Ở đây có truyện hay. Mại dô, mại dô, mại dô!</p>
-      </div>
-    </aside>
-  );
-}
