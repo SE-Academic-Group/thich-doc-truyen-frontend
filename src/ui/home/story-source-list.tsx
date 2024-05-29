abade3298a429561842adb99094c4e1f205fb06b
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-29 15:07:55 +0700

    chore: add aria-live and change util name (convertToCapitalize -> capitalize)

--- src/ui/home/story-source-list.tsx
+++ src/ui/home/story-source-list.tsx
@@ -1,5 +1,5 @@
-import { getPluginList } from "@/lib/data/get-plugin-list";
-import { convertToCapitalCase } from "@/lib/utils";
+import { getPluginList } from "@/data/get-plugin-list";
+import { capitalize } from "@/lib/utils";
 import Link from "next/link";
 
 export default async function StorySourceList() {
@@ -9,6 +9,7 @@ export default async function StorySourceList() {
     return (
       <p
         role="alert"
+        aria-live="polite"
         className="rounded-sm bg-red-100 px-1 text-sm text-red-800"
       >
         Có lỗi xảy ra trong quá trình hiển thị danh sách nguồn truyện
@@ -27,7 +28,7 @@ export default async function StorySourceList() {
               rel="no-referer no-opener"
               className="text-fg-700 rounded bg-bg-50 px-3 py-1.5 text-sm hover:underline"
             >
-              {convertToCapitalCase(plugin.pluginName)}
+              {capitalize(plugin.pluginName)}
             </Link>
           </li>
         ))}
