2b996034b49ce2b8042d7817e15d16f0beaa72a0
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-30 16:25:49 +0700

    style: responsive footer

--- src/ui/layout/footer.tsx
+++ src/ui/layout/footer.tsx
@@ -4,8 +4,8 @@ export default function Footer() {
   return (
     <footer className="bg-bg-50">
       <div className="container flex flex-wrap items-center justify-between gap-4 py-4">
-        <ThichDocTruyenLogo size="sm" />
-        <p className="max-w-prose text-pretty text-center text-sm">
+        <ThichDocTruyenLogo size="sm" className="hidden sm:block" />
+        <p className="max-w-prose text-pretty text-sm md:text-center">
           Đây chỉ là website phục vụ nhu cầu đồ án môn học. Chúng tôi không có
           mục đích thương mại cạnh tranh với bất kỳ trang web nào khác.
         </p>
