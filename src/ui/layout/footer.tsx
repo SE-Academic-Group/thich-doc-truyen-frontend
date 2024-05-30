08dcee4a08405d75c2a6fbe60963b77a530e5ece
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-30 15:22:17 +0700

    style: make padding on y axis smaller

--- src/ui/layout/footer.tsx
+++ src/ui/layout/footer.tsx
@@ -3,7 +3,7 @@ import ThichDocTruyenLogo from "../common/thich-doc-truyen-logo";
 export default function Footer() {
   return (
     <footer className="bg-bg-50">
-      <div className="container flex flex-wrap items-center justify-between gap-4 py-6">
+      <div className="container flex flex-wrap items-center justify-between gap-4 py-4">
         <ThichDocTruyenLogo size="sm" />
         <p className="max-w-prose text-pretty text-center text-sm">
           Đây chỉ là website phục vụ nhu cầu đồ án môn học. Chúng tôi không có
