4f6763982002a81f43a0d35953953c918c190e9d
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-30 15:26:55 +0700

    refactor: handle this search completely on client side

--- src/ui/layout/header-search-form.tsx
+++ src/ui/layout/header-search-form.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import { searchAction } from "@/lib/actions";
-import { useEffect, useRef } from "react";
-import { useFormState } from "react-dom";
+import { cn } from "@/lib/utils";
+import { searchQuerySchema } from "@/types/search-query";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState, useTransition } from "react";
 import { SearchIcon } from "../../lib/icons";
-import Kbd from "../common/kbd";
 import ErrorText from "../common/error-text";
+import Kbd from "../common/kbd";
 
 export default function HeaderSearchForm() {
-  const [state, action] = useFormState(searchAction, { error: "" });
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const [error, setError] = useState<string | null>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
@@ -25,25 +28,51 @@ export default function HeaderSearchForm() {
     };
   }, []);
 
+  const onSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const result = searchQuerySchema.safeParse(inputRef.current?.value);
+
+    if (result.error) {
+      return setError(result.error.format()._errors.join(", "));
+    }
+
+    startTransition(() => {
+      setError(null);
+      router.push(`/tim-kiem?q=${result.data}`);
+    });
+  };
+
   return (
-    <form action={action}>
+    <form onSubmit={onSearch}>
       <label htmlFor="search-input" className="sr-only">
         Tìm kiếm truyện theo từ khóa
       </label>
-      <div className="relative flex items-center rounded border border-border bg-bg-0 px-2">
+      <div
+        className={cn(
+          "relative flex items-center rounded border border-border bg-bg-0 px-2",
+          isPending && "bg-bg-50"
+        )}
+      >
         <SearchIcon className="pointer-events-none size-[14px] text-fg-500" />
         <input
           type="search"
-          name="search-input"
           id="search-input"
-          className="w-full px-4 py-1 placeholder:text-sm focus:outline-none"
+          className="w-full bg-transparent px-4 py-1 placeholder:text-sm focus:outline-none"
           placeholder="Tìm kiếm theo từ khóa..."
           ref={inputRef}
+          disabled={isPending}
         />
         <Kbd keystrokes={["/"]} screenReaderText="Nhấn phím để tìm kiếm" />
-        <div className="absolute inset-x-0 top-[110%] bg-bg-50 p-0.5 shadow">
-          <ErrorText>{state.error}</ErrorText>
-        </div>
+        {error && (
+          <div className="absolute inset-x-0 top-[115%] border bg-bg-0 p-1 shadow-md">
+            <ErrorText>{error}</ErrorText>
+          </div>
+        )}
+        {isPending && (
+          <span className="sr-only" aria-live="assertive">
+            Đang xử lý kết quả tìm kiếm với từ khóa {inputRef.current?.value}
+          </span>
+        )}
       </div>
     </form>
   );
