2e38e26ec24322d71c23df7d3800ad0d75da047e
Truong Minh Phat <tmphat1312@gmail.com>
2024-05-31 16:40:52 +0700

    feat: add previous and next pagination button

--- src/ui/common/pagination-link.tsx
+++ src/ui/common/pagination-link.tsx
@@ -1,18 +1,21 @@
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 
-export type PaginationLinkProps = {
-  page: number | "...";
-  url: string;
-  isActive: boolean;
-  scrollToId?: string;
-};
+export type PaginationLinkProps = Readonly<
+  React.PropsWithChildren & {
+    page: number | "...";
+    url: string;
+    isActive: boolean;
+    scrollToId?: string;
+  }
+>;
 
 export function PaginationLink({
   url,
   page,
   isActive,
   scrollToId,
+  children,
 }: PaginationLinkProps) {
   if (page === "...") {
     return <span className="text-lg md:text-base">{page}</span>;
@@ -28,13 +31,13 @@ export function PaginationLink({
   return (
     <Link
       className={cn(
-        "bg-bg-200 hover:bg-bg-300 rounded-sm",
+        "bg-bg-100 hover:bg-bg-200 rounded-sm",
         "md:px-2.5 md:py-1 md:text-base px-3 py-2 text-lg",
         isActive && "bg-primary text-fg-900 pointer-events-none"
       )}
       href={paginationUrl.toString()}
     >
-      {page}
+      {children}
     </Link>
   );
 }
