4a2dde897ecdb60d50176f1c7b35d9d04ac18769
Truong Minh Phat <tmphat1312@gmail.com>
2024-06-05 13:51:30 +0700

    refactor: provide a single source of truth of ui

--- src/ui/fonts.ts
+++ src/ui/fonts.ts
@@ -1,37 +1,33 @@
 import {
   Inconsolata,
-  Literata,
   Lora,
-  Noto_Serif,
   Open_Sans,
   Protest_Revolution,
   Roboto_Condensed,
 } from "next/font/google";
 
-const robotoCondensed = Roboto_Condensed({
+export const robotoCondensed = Roboto_Condensed({
   subsets: ["vietnamese"],
   variable: "--font-roboto-condensed",
 });
 
-const protestRevolution = Protest_Revolution({
+export const protestRevolution = Protest_Revolution({
   weight: ["400"],
   subsets: ["vietnamese"],
   variable: "--font-protest-revolution",
 });
 
-const lora = Lora({
+export const lora = Lora({
   subsets: ["vietnamese"],
   variable: "--font-lora",
 });
 
-const openSans = Open_Sans({
+export const openSans = Open_Sans({
   subsets: ["vietnamese"],
   variable: "--font-open-sans",
 });
 
-const inconsolata = Inconsolata({
+export const inconsolata = Inconsolata({
   subsets: ["vietnamese"],
   variable: "--font-inconsolata",
 });
-
-export const fontVariables = `${robotoCondensed.variable} ${protestRevolution.variable} ${lora.variable} ${openSans.variable} ${inconsolata.variable}`;
