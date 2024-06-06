581496ed832755ce5ee366daefff8497bb9b9fc0
Truong Minh Phat <tmphat1312@gmail.com>
2024-06-06 20:08:09 +0700

    style: make responsiv easier

--- src/app/(with-layout)/chi-tiet/story-text.tsx
+++ src/app/(with-layout)/chi-tiet/story-text.tsx
@@ -1,4 +1,5 @@
 import ReadMore from "../../../ui/common/read-more";
+import { cn } from "@/lib/utils";
 import { TStoryDetail } from "@/types/story-detail";
 import Image from "next/image";
 
@@ -7,61 +8,70 @@ type StoryTextProps = {
 };
 
 export default function StoryText({ storyText }: StoryTextProps) {
+  const poster = (
+    <Image
+      src={storyText.image}
+      alt={storyText.title}
+      width={200}
+      height={300}
+      className="mx-auto aspect-[2/3] w-[200px] rounded bg-bg-300 object-cover shadow md:mx-0"
+    />
+  );
+
+  const title = (
+    <h3 className="text-xl font-semibold uppercase md:text-pretty">
+      {storyText.title}
+    </h3>
+  );
+
+  const desc = (
+    <div className="text-start">
+      <hr className="my-2.5 border" />
+      <div className="whitespace-pre-line text-pretty text-sm leading-6">
+        <ReadMore>{storyText.description}</ReadMore>
+      </div>
+      <hr className="mt-2.5 border" />
+    </div>
+  );
+
+  const info = (
+    <ul
+      className={cn(
+        "mixin:inline-block mixin:pe-1.5 mixin:font-semibold",
+        "space-y-1 mx-auto max-w-[15rem] text-sm",
+        "md:flex md:max-w-none md:items-center md:mixin:shrink-0 md:flex-wrap md:gap-2 md:justify-center md:space-y-0",
+      )}
+    >
+      <li>
+        <span className="mixin">Tác giả: </span>
+        <span>{storyText.author}</span>
+      </li>
+      <li>
+        <span className="mixin">Số chương: </span>
+        <span>{storyText.nchapter}</span>
+      </li>
+      <li>
+        <span className="mixin">Thể loại: </span>
+        <span>{storyText.genres.join(", ")}</span>
+      </li>
+    </ul>
+  );
+
   return (
-    <article className="grid justify-center gap-4 rounded-lg px-1.5 py-2 md:grid-cols-[repeat(2,auto)] md:gap-12">
-      <section className="flex flex-col gap-3 text-center md:text-start">
-        <h3 className="mb-2  text-xl font-semibold md:hidden">
-          {storyText.title}
-        </h3>
-        <Image
-          src={storyText.image}
-          alt={storyText.title}
-          width={200}
-          height={300}
-          className="mx-auto aspect-[2/3] w-[200px] rounded bg-bg-300 object-cover shadow md:mx-0"
-        />
-        <section className="mx-auto max-w-[200px] text-sm md:hidden">
-          <ul className="space-y-1 mixin:inline-block mixin:pe-1.5 mixin:font-semibold">
-            <li>
-              <span className="mixin">Tác giả: </span>
-              <span>{storyText.author}</span>
-            </li>
-            <li>
-              <span className="mixin">Thể loại: </span>
-              <span>{storyText.genres.join(", ")}</span>
-            </li>
-            <li>
-              <span className="mixin">Số chương: </span>
-              <span>{storyText.nchapter}</span>
-            </li>
-          </ul>
-        </section>
+    <article className="gap-4 rounded-lg px-1.5 py-2">
+      <section className="text-center space-y-3 md:hidden">
+        {title}
+        {poster}
+        {info}
+        {desc}
       </section>
-      <section className="max-w-prose">
-        <h3 className="mb-2.5 hidden  text-center text-2xl font-semibold uppercase md:block">
-          {storyText.title}
-        </h3>
-        <section className="hidden text-sm md:block">
-          <ul className="flex flex-wrap justify-center gap-x-3 mixin:inline-block mixin:pe-1.5 mixin:font-semibold">
-            <li>
-              <span className="mixin">Tác giả: </span>
-              <span>{storyText.author}</span>
-            </li>
-            <li>
-              <span className="mixin">Số chương: </span>
-              <span>{storyText.nchapter}</span>
-            </li>
-            <li>
-              <span className="mixin">Thể loại: </span>
-              <span>{storyText.genres.join(", ")}</span>
-            </li>
-          </ul>
-        </section>
-        <hr className="my-2.5 border" />
-        <div className="whitespace-pre-line text-pretty text-sm leading-6">
-          <ReadMore>{storyText.description}</ReadMore>
+      <section className="gap-8 text-center hidden md:grid grid-cols-[auto_1fr]">
+        <div className="pt-8">{poster}</div>
+        <div className="space-y-2.5">
+          {title}
+          {info}
+          {desc}
         </div>
-        <hr className="mb-5 mt-2.5 border" />
       </section>
     </article>
   );
