abfb51e8faa871675ef87fcea619155ee2f0d9ad
Truong Minh Phat <tmphat1312@gmail.com>
2024-06-08 21:33:02 +0700

    refactor: Update LayoutProps type to PropsWithChildren
    This commit updates the type of LayoutProps in the layout.tsx file from React.WithChildren to React.PropsWithChildren. This change ensures that the PropsWithChildren type is used consistently throughout the codebase, improving code clarity and maintainability.

--- src/app/doc-truyen/layout.tsx
+++ src/app/doc-truyen/layout.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/app/doc-truyen/_layout/footer";
 import Header from "@/app/doc-truyen/_layout/header";
 
-type LayoutProps = React.WithChildren;
+type LayoutProps = React.PropsWithChildren;
 
 export default function Layout({ children }: LayoutProps) {
   return (
