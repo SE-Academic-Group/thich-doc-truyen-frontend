787e6f34228d4524d2759c08eab1ae30429d1ede
Truong Minh Phat <tmphat1312@gmail.com>
2024-06-08 12:16:23 +0700

    feat: Add SaveToLocal component for downloading chapters (MOCK)

--- src/app/doc-truyen/_layout/content.tsx
+++ src/app/doc-truyen/_layout/content.tsx
@@ -4,6 +4,7 @@ import ChangeChapterSource from "../_components/change-chapter-source";
 import ChapterNavigation from "../_components/chapter-navigation";
 import CurrentSource from "../_components/current-source";
 import ReadingPadSettings from "../_components/reading-pad-settings";
+import SaveToLocal from "../_components/save-to-local";
 import SetReadingHistory from "../_components/set-reading-history";
 import SettingsConsumer from "../_components/settings-consumer";
 import StoryContent from "../_components/story-content";
@@ -33,9 +34,12 @@ export default function Content({
         <SettingsConsumer>
           <article className="container">
             <section className="container py-3">
-              <div className="flex justify-between mb-2">
+              <div className="flex justify-between mb-2.5">
                 <CurrentSource />
-                <ReadingPadSettings />
+                <div className="space-x-3.5">
+                  <SaveToLocal chapterURL={chapterDetail.url} />
+                  <ReadingPadSettings />
+                </div>
               </div>
               <h3 className="mb-2 text-center font-medium uppercase text-fg-500 sm:text-lg">
                 <Link
