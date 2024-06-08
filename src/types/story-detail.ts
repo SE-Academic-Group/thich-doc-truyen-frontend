b54ab7f4f2c91a5608b7c23136219dde1a38717b
Truong Minh Phat <tmphat1312@gmail.com>
2024-06-08 11:13:34 +0700

    refactor: transform double line breaks in story description

--- src/types/story-detail.ts
+++ src/types/story-detail.ts
@@ -1,11 +1,15 @@
 import { z } from "zod";
 
+const removeDoubleLineBreak = (val: string) => {
+  return val.replace(/(<br>){2}/g, "<br>");
+};
+
 export const ZStoryDetail = z.object({
   title: z.string(),
   author: z.string(),
   image: z.string().url(),
   url: z.string().url(),
-  description: z.string(),
+  description: z.string().transform(removeDoubleLineBreak),
   genres: z.array(z.string()),
   nchapter: z.number().int().min(0),
 });
